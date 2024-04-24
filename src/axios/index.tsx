5649cd5c340272ca31111353507a43d5363d7470
COsta <kostyalasiy@gmail.com>
2024-04-24 11:02:54 +0900

    added dev backendurl

--- src/axios/index.tsx
+++ src/axios/index.tsx
@@ -1,6 +1,8 @@
 import axios from "axios";
-const BACKEND_URL = import.meta.env.VITE_API_BACKEND;
-
+const DEV = import.meta.env.DEV;
+const BACKEND_URL = DEV
+  ? "http://127.0.0.1:8000"
+  : import.meta.env.VITE_API_BACKEND;
 
 export const axiosInstance = axios.create({
   baseURL: BACKEND_URL,
