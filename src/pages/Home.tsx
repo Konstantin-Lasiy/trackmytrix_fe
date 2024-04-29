ba6a1aa49f511a92ec61ae0d2c096dbed2417606
COsta <kostyalasiy@gmail.com>
2024-04-28 23:37:10 -0600

    added backendurl to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,10 +4,12 @@ import "./Home.css"; // Import the CSS file
 import SignIn from "../components/auth/LoginForm";
 import useAuth from "../hooks/useAuth";
 
+const BACKEND_URL = import.meta.env.VITE_API_BACKEND;
 const HomePage = () => {
   const { accessToken } = useAuth();
   return (
     <div id="home">
+      <li> {BACKEND_URL} </li>
       <li> Click on Create Run </li>
       <li> Toggle options for a trick </li>
       <li> Click on Trick name to add it to the timeline </li>
