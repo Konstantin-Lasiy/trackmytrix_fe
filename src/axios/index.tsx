03cb8d10f0ec52f295e4e7d2a193293efb5131f9
COsta <kostyalasiy@gmail.com>
2024-04-28 23:29:16 -0600

    updated dev logic in axios index

--- src/axios/index.tsx
+++ src/axios/index.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
-const DEV = import.meta.env.DEV;
+const DEV = import.meta.env.DEV ? true : false;
+
 const BACKEND_URL = DEV
   ? "http://127.0.0.1:8000"
   : import.meta.env.VITE_API_BACKEND;
