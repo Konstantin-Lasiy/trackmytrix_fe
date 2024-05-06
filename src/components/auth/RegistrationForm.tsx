fb70bd13f806b4305f19dd4e9dfa826620827fa3
COsta <kostyalasiy@gmail.com>
2024-05-06 12:10:19 +0300

    visual feedback and errors to registration

--- src/components/auth/RegistrationForm.tsx
+++ src/components/auth/RegistrationForm.tsx
@@ -11,37 +11,52 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { Link as RouterLink } from "react-router-dom";
-import { Link } from "@mui/material";
+import { Link, Alert } from "@mui/material";
+import { AxiosError } from "axios";
+
+interface Errors {
+  [key: string]: string;
+}
 
 interface RegistrationResponse {
-  key: string; 
+  key: string;
 }
 
 const RegistrationForm: React.FC = () => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
+  const [errors, setErrors] = useState<Record<string, string>>({});
 
   const handleRegistrationSuccess = (responseData: RegistrationResponse) => {
-    alert("Registration successful! You can now log in.");
     console.log(responseData);
-    navigate("/login");
+    navigate('/login', { state: { registered: true } });
   };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
+    setLoading(true);
+    const request = JSON.stringify({
+      username: data.get("username"),
+      email: data.get("email"),
+      password: data.get("password"),
+      password2: data.get("password2"),
+    });
     try {
-      setLoading(true);
-      const request = JSON.stringify({
-        username: data.get("username"),
-        email: data.get("email"),
-        password: data.get("password"),
-        password2: data.get("password2"),
-      });
       const response = await axiosInstance.post("auth/register", request);
       handleRegistrationSuccess(response.data);
-    } catch (error) {
+    } catch (error: unknown) {
       console.error("Registration failed", error);
+      if (error instanceof AxiosError) {
+        const errMessages: Errors = error.response?.data || {
+          generic: "Registration failed, please try again.",
+        };
+        setErrors(errMessages);
+      } else {
+        setErrors({ generic: "Registration failed, please try again." });
+      }
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -79,6 +94,8 @@ const RegistrationForm: React.FC = () => {
                   id="username"
                   label="Username"
                   autoFocus
+                  error={!!errors.username}
+                  helperText={errors.username}
                 />
               </Grid>
               <Grid item xs={8}>
@@ -89,6 +106,8 @@ const RegistrationForm: React.FC = () => {
                   label="Email"
                   name="email"
                   autoComplete="email"
+                  error={!!errors.email}
+                  helperText={errors.email}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -100,6 +119,8 @@ const RegistrationForm: React.FC = () => {
                   type="password"
                   id="password"
                   autoComplete="new-password"
+                  error={!!errors.password}
+                  helperText={errors.password}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -111,15 +132,33 @@ const RegistrationForm: React.FC = () => {
                   type="password"
                   id="password2"
                   autoComplete="new-password"
+                  error={!!errors.password2}
+                  helperText={errors.password2}
                 />
               </Grid>
             </Grid>
+            {Object.keys(errors).length > 0 && (
+              <Box sx={{ width: "100%", mt: 2 }}>
+                {Object.entries(errors)
+                  .filter(
+                    ([key]) =>
+                      !["username", "email", "password", "password2"].includes(
+                        key
+                      )
+                  )
+                  .map(([key, value]) => (
+                    <Alert severity="error" key={key}>
+                      {key} : {value}
+                    </Alert>
+                  ))}
+              </Box>
+            )}
             <Button
               type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
-              //disabled={loading}
+              disabled={loading}
             >
               {loading ? "Registering..." : "Sign Up"}
             </Button>
